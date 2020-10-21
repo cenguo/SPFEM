5e023f148f32a4306e20492f03c084f056ef7eb5
Ning Guo <hhh.guo@gmail.com>
2020-10-21 10:05:31 +0800

    Add files via upload

---
+++ column.py
@@ -0,0 +1,71 @@
+from spfem_mfront import Plasticity
+import numpy as np
+import stripy
+
+lx = .1; ly = .1; g = [0, -10.]
+rho = 2650.; mu = np.tan(19.8/180*np.pi)
+mater = {
+    'rho':rho,
+    'young':1.5e6,
+    'poisson':0.3,
+    'mu':mu,
+    'coh':0.,
+    'gs':1.e-4,
+    'alpha':0.025,
+    'eps0':1.0,
+    'vz':.5,
+    'c0':0.025,
+    'chi_inf':0.08,
+    'chi0':0.04,
+}
+
+space = ly / 50.
+base_height = 0.#space * 2
+base_length = lx * 2.5
+np.random.seed(12345)
+mesh = stripy.cartesian_meshes.square_mesh([0., lx, base_height, ly + base_height], space, space, 0.3)
+#base = stripy.cartesian_meshes.square_mesh([0., base_length, 0., base_height], space, space, 0)
+#pts = np.column_stack((np.append(mesh.x,base.x), np.append(mesh.y,base.y)))
+pts = np.column_stack((mesh.x, mesh.y))
+npts = len(pts)
+print('space = %f'%space)
+print('npts = %d'%npts)
+charlen = 1.5 * space
+
+t_inf = np.sqrt(ly / -g[1]) * 4.
+sigv = mater['rho'] * g[1] * ((ly + base_height - pts[:,1]) * (pts[:,0] < lx + 1.e-6) + (base_height - pts[:,1]) * (pts[:,0] > lx + 1.e-6))
+k0 = 1. - np.sin(np.arctan(mater['mu'])); print('k0 =',k0)
+sigh = k0 * sigv
+sigma = np.column_stack([sigh, sigv, sigh, np.zeros(npts)])
+prob = Plasticity(pts,mater,sigma,gravity=g,charlen=charlen)
+dt0 = prob.getTimeStepSize()
+print('Initial time step size:',dt0,'t_inf: ',t_inf)
+print('Time steps estimated:',int(t_inf / dt0))
+sig = prob.getStress()
+eps = prob.getStrain()
+chi = prob.getCompactivity()
+vp = prob.getPlasticStrain()
+disp = prob.getNodalDisp()
+vel = prob.getNodalVel()
+prob.saveMeshVar(name_prefix='./result/step_0_',disp=disp,vel=vel,sig=sig,eps=eps,chi=chi,vp=vp)
+
+t = 0.
+left_pts = (pts[:,0] < 1.e-6)
+left_msk = np.outer(left_pts, [True, False]).reshape(-1)
+prob.initTimeStep(Dmsk=left_msk,FricBound=np.array([[[-lx/10., 0.],[lx*3., 0.]],]),mu=[mu,])
+
+tstep = [t_inf/200*i for i in range(1,211)]
+step = 0; rem = 0
+while (t < t_inf):
+    dt = prob.getTimeStepSize()
+    t += dt
+    du = prob.solve()
+    if t >= tstep[step]:
+        step += 1
+        sig = prob.getStress()
+        eps = prob.getStrain()
+        chi = prob.getCompactivity()
+        vp = prob.getPlasticStrain()
+        disp = prob.getNodalDisp()
+        vel = prob.getNodalVel()
+        prob.saveMeshVar(name_prefix='./result/step_%d_'%step,disp=disp,vel=vel,sig=sig,eps=eps,chi=chi,vp=vp)
